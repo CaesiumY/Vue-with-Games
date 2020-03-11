c57c06c52a41d63cce139f7be5f90e16b8d66fa3
caesiumy <mn0658@naver.com>
2020-03-11 21:31:48 +0900

    feat: Init 4. ResponseCheck

---
+++ 4. 반응속도체크/ResponseCheck.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="screen"></div>
+    <div>
+      <div>평균시간 {{}}</div>
+      <button @click="onReset">리셋</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    onReset() {}
+  }
+};
+</script>
+
+<style scoped></style>
