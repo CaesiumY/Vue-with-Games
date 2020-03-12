ae3773f6df395fbe9a9fcb60b15315d07f4224f9
caesiumy <mn0658@naver.com>
2020-03-13 00:03:20 +0900

    fix: Fix type property error

--- 5. 가위바위보/main.js
+++ 5. 가위바위보/main.js
@@ -1,4 +1,9 @@
 import Vue from "vue";
 import RockScissorsPaper from "./RockScissorsPaper";
 
-new Vue(RockScissorsPaper).$mount("#app");
+// new Vue(RockScissorsPaper).$mount("#app");
+
+new Vue({
+  el: "#app",
+  render: h => h(RockScissorsPaper)
+});
