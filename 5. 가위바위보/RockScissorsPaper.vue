94e3e6154c190b7b75337462b63f247322e7a1e7
caesiumy <mn0658@naver.com>
2020-03-12 23:42:39 +0900

    feat: Init RockScissorsPaper

---
+++ 5. 가위바위보/RockScissorsPaper.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div
+      id="computer"
+      style="background: url(`http://en.pimg.jp/023/182/267/1/23182267.jpg` 0 0)"
+    >
+      hello
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#computer {
+  width: 142px;
+  height: 200px;
+  background-position: 0 0;
+}
+</style>
