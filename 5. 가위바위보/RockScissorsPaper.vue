131d2a23dc38e926060f047bee98451420e8c1de
caesiumy <mn0658@naver.com>
2020-03-14 01:12:12 +0900

    feat: Add img change position logic at mounted

--- 5. 가위바위보/RockScissorsPaper.vue
+++ 5. 가위바위보/RockScissorsPaper.vue
@@ -1,16 +1,53 @@
 <template>
   <div>
-    <div
-      id="computer"
-      style="background: url(`http://en.pimg.jp/023/182/267/1/23182267.jpg` 0 0)"
-    >
-      hello
+    <div id="computer" :style="computedStyleObject"></div>
+    <div>
+      <button>바위</button>
+      <button>가위</button>
+      <button>보</button>
+    </div>
+    <div>
+      <div>{{ result }}</div>
+      <div>총 {{ score }} 점</div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+const coordList = {
+  rock: "0",
+  scissor: "-141px",
+  paper: "-282px"
+};
+
+let interval = null;
+
+export default {
+  data() {
+    return {
+      imgCoord: coordList.rock,
+      result: "",
+      score: 0,
+      selected: 0
+    };
+  },
+  computed: {
+    computedStyleObject() {
+      return {
+        background: `url(http://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`
+      };
+    }
+  },
+  mounted() {
+    interval = setInterval(() => {
+      this.selected = (this.selected + 1) % Object.keys(coordList).length;
+      this.imgCoord = Object.values(coordList)[this.selected];
+    }, 100);
+  },
+  beforeDestroy() {
+    clearInterval(interval);
+  }
+};
 </script>
 
 <style scoped>
