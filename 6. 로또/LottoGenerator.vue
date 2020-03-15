183ca2731de6987c44a0296dcd1f258b69cd4340
caesiumy <mn0658@naver.com>
2020-03-15 22:08:20 +0900

    feat: Complete LottoGenerator

--- 6. 로또/LottoGenerator.vue
+++ 6. 로또/LottoGenerator.vue
@@ -1,9 +1,75 @@
 <template>
-  <div></div>
+  <div>
+    <div>로또 생성기</div>
+
+    <div id="lottoBall">
+      <lotto-ball v-for="ball in balls" :key="ball" :number="ball"></lotto-ball>
+    </div>
+
+    <div>보너스</div>
+    <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+    <button v-if="re" @click="onClickRestart">한 번 더!</button>
+  </div>
 </template>
 
 <script>
-export default {};
+import LottoBall from "./components/LottoBall";
+
+const timeouts = [];
+
+export default {
+  components: {
+    LottoBall
+  },
+  data() {
+    return {
+      ballList: this.getRandomLotto(),
+      balls: [],
+      bonus: null,
+      re: false
+    };
+  },
+  methods: {
+    getRandomLotto() {
+      const nums = [];
+      for (let i = 0; nums.length < 7; i++) {
+        const randomNum = Math.ceil(Math.random() * 45);
+        if (!nums.includes(randomNum)) {
+          nums.push(randomNum);
+        }
+      }
+      return nums;
+    },
+    onClickRestart() {
+      this.ballList = this.getRandomLotto();
+      this.balls = [];
+      this.bonus = null;
+      this.re = false;
+
+      this.showLottoBalls();
+    },
+    showLottoBalls() {
+      for (let i = 0; i < this.ballList.length - 1; i++) {
+        timeouts[i] = setTimeout(() => {
+          this.balls.push(this.ballList[i]);
+        }, (i + 1) * 1000);
+      }
+
+      timeouts[this.ballList.length - 1] = setTimeout(() => {
+        this.bonus = this.ballList[this.ballList.length - 1];
+        this.re = true;
+      }, this.ballList.length * 1000);
+    }
+  },
+  mounted() {
+    this.showLottoBalls();
+  },
+  beforeDestroy() {
+    timeouts.forEach(item => {
+      clearTimeout(item);
+    });
+  }
+};
 </script>
 
 <style scoped></style>
--- 6. 로또/components/LottoBall.vue
+++ 6. 로또/components/LottoBall.vue
@@ -1,9 +1,43 @@
 <template>
-  <div></div>
+  <div class="ball" :style="lottoBallStyle">{{ number }}</div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    number: Number
+  },
+  computed: {
+    lottoBallStyle() {
+      let background;
+      if (this.number <= 10) {
+        background = "#ff7675";
+      } else if (this.number <= 20) {
+        background = "#ffeaa7";
+      } else if (this.number <= 30) {
+        background = "#81ecec";
+      } else if (this.number <= 40) {
+        background = "#74b9ff";
+      } else {
+        background = "#a29bfe";
+      }
+
+      return { background };
+    }
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.ball {
+  display: inline-block;
+  border: 1px solid #000;
+  border-radius: 20px;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  font-size: 20px;
+  text-align: center;
+  margin-left: 20px;
+}
+</style>
