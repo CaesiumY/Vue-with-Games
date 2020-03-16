393de456ba8cce08f2156f478cc66509c00eaedc
caesiumy <mn0658@naver.com>
2020-03-16 23:44:37 +0900

    feat: Init 7-1 TicTacToe ver. EventBus

---
+++ 7-1 틱택토-EventBus/TicTacToe.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>현재 {{ nowTurn }}님의 턴입니다.</div>
+    <table-component :table-data="tableData"></table-component>
+    <div v-if="winner">{{ winner }}님의 승리입니다!</div>
+  </div>
+</template>
+
+<script>
+import TableComponent from "./components/TableComponent";
+export default {
+  components: {
+    TableComponent
+  },
+  data() {
+    return {
+      tableData: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+      ],
+      nowTurn: "O",
+      winner: ""
+    };
+  }
+};
+</script>
+
+<style>
+table {
+  border-collapse: collapse;
+}
+
+td {
+  border: 1px solid black;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+}
+</style>
---
+++ 7-1 틱택토-EventBus/main.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import TicTacToe from "./TicTacToe";
+
+// new Vue(RockScissorsPaper).$mount("#app");
+
+new Vue(TicTacToe).$mount("#app");
