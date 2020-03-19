622e27ee1b4d91bd21dd445957f62063cf792f5c
caesiumy <mn0658@naver.com>
2020-03-19 23:36:06 +0900

    feat: Init 8th project MineSweeper

---
+++ 8. 지뢰찾기/main.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import MineSweeper from "./MineSweeper";
+
+// new Vue(RockScissorsPaper).$mount("#app");
+
+new Vue(MineSweeper).$mount("#app");
---
+++ 8. 지뢰찾기/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".vue"]
+  },
+  entry: {
+    app: path.join(__dirname, "main.js")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: ["vue-style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [new VueLoaderPlugin()],
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+    publicPath: "/dist"
+  }
+};
