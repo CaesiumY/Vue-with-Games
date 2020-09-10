cfc4cce74d1ce357540aba6f0fd05736d10dcb80
caesiumy <mn0658@naver.com>
2020-09-10 23:12:30 +0900

    style: Update suitcolor logic & deck style

--- 9. 덱셔플/main.js
+++ 9. 덱셔플/main.js
@@ -4,6 +4,12 @@ new Vue({
     ranks: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
     suits: ["♥", "♦", "♠", "♣"],
     cards: [],
+    suitColor: {
+      "♠": "black",
+      "♣": "black",
+      "♦": "red",
+      "♥": "red",
+    },
   },
   created() {
     this.displayInitialDeck();
